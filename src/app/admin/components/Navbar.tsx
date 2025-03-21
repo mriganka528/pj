b75cf8e439237180c40cce5b994dd2dbf971422e
Mriganka Sarma <mrigankasarma528@gmail.com>
2025-03-21 15:23:25 +0530

    ui updated

--- src/app/admin/components/Navbar.tsx
+++ src/app/admin/components/Navbar.tsx
@@ -7,7 +7,8 @@ import { LayoutDashboard, FileText, Calendar, Settings, User } from "lucide-reac
 import { UserButton } from "@clerk/nextjs"
 import { ModeToggle } from "@/components/ModeToggle"
 import Sidebar from "./Sidebar"
-
+import { useMotionValueEvent, useScroll, motion } from "framer-motion"
+import { useState } from "react"
 const navItems = [
     { name: "Dashboard", href: "/admin", icon: LayoutDashboard },
     { name: "Content", href: "/admin/content", icon: FileText },
@@ -18,9 +19,24 @@ const navItems = [
 
 const Navbar = () => {
     const pathname = usePathname()
+    const { scrollY } = useScroll();
+    const [hidden, setHidden] = useState(false);
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        const previous = scrollY.getPrevious() ?? 0;
+        if (latest > previous && latest > 90) {
+            setHidden(true);
+        } else {
+            setHidden(false);
+        }
+    });
 
     return (
-        <nav className=" text-sm sm:text-base bg-background border-b w-svw flex justify-between px-6 py-5 sm:py-6 shadow-sm dark:border-border">
+        <motion.nav variants={{
+            visible: { y: 0 },
+            hidden: { y: "-100%" },
+        }}
+            animate={hidden ? "hidden" : "visible"}
+            transition={{ duration: 0.35, ease: "easeInOut" }} className=" text-sm sm:text-base bg-background border-b w-svw flex justify-between px-6 py-5 sm:py-6 shadow-sm dark:border-border">
             <Sidebar />
             <div >
                 <h1 className=" max-[768px]:hidden  text-xl md:text-2xl font-bold text-foreground">Admin Control Center</h1>
@@ -51,7 +67,7 @@ const Navbar = () => {
                 <ModeToggle />
                 <UserButton />
             </div>
-        </nav>
+        </motion.nav>
     )
 }
 
--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -3,7 +3,6 @@ import UserManagement from './componenets/UserManagement'
 import prismadb from '@/lib/prismadb'
 async function page() {
     const getSuscribedUsers = await prismadb.suscribedUser.findMany()
-    console.log(getSuscribedUsers)
     return (
         <main className="container mx-auto px-4 py-8">
             <h1 className="text-3xl font-bold mb-8">Notice Board Subscription Management</h1>
