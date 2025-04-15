10e3dfef8613396d9be9c426476c7e6a2c25c737
Mriganka Sarma <mrigankasarma528@gmail.com>
2025-04-15 09:53:52 +0530

    signUp layout changed

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { UserPen } from "lucide-react";
 import Image from "next/image";
 
 export default function AuthLayout({
@@ -6,12 +7,31 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex justify-center items-start sm:items-center h-screen">
-      <div className=" sm:border-2 sm:shadow-md sm:rounded-lg flex flex-col p-10 justify-start pt-14 sm:justify-center space-y-8  items-center " >
-        <h1 className="text-center text-3xl font-bold">Sign In To BulletinX</h1>
-        <div className="flex flex-col sm:flex-row items-center justify-center space-y-5 sm:space-x-8 ">
-          <Image src={"/assets/undraw-login.svg"} className=" h-[10rem] w-[10rem] sm:h[15rem] sm:w-[15rem] md:h-[20rem] md:w-[20rem]" height={300} width={400} alt="Login image" />
-          {children}
+
+    <div className="w-svw h-svh bg-signUP  flex fleex-col md:flex-row flex-col items-center justify-center  ">
+      <div className="w-svw md:w-[55%] h-[50%] md:h-full flex flex-col items-center shadow-lg bg-gray-50 rounded-b-[40%] md:rounded-l-[0%] md:rounded-r-[25%] ">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mt-7 md:mt-10 text-center text-gray-700 ">BulletinX</h1>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mt-14 md:mt-20 bg-[linear-gradient(116deg,_#222F3D,_#bdc4e5)] bg-clip-text text-transparent ">
+          Ready to Power Up BulletinX?
+        </h1>
+        <div className=" max-[768px]:hidden flex flex-col  my-5 md:mt-20 pl-9">
+          <p className=" text-base md:text-2xl text-start mt-5 md:mt-10 text-gray-600">
+            Sign up as an Admin and start shaping the information flow.
+          </p>
+          <p className=" text-base md:text-2xl text-start mt-5 md:mt-10 text-gray-600">
+            From managing notices to engaging users — you&apos;re in charge of it all.
+          </p>
+          <p className=" text-base md:text-2xl text-start mt-5 md:mt-10 text-gray-600">
+            Your dashboard is waiting. Let&apos;s build something impactful.
+          </p>
+        </div>
+      </div>
+      <div className="md:w-[45%]  h-full flex md:items-center justify-center ">
+        <div className=" h-[80%] w-[80%] flex flex-col space-y-5 mt-10 md:mt-0 md:items-center justify-start md:justify-center">
+          <h1 className="text-center w-full text-4xl font-bold text-gray-700"> <UserPen className="inline-block h-8 w-8" />  Sign In</h1>
+          <div className="w-full flex justify-center items-center">
+            {children}
+          </div>
         </div>
       </div>
     </div>
